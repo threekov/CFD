4deabb74c95bc70c13213283b3d27a5dbcec865c
threekov <161291961+threekov@users.noreply.github.com>
2024-02-28 01:30:14 +0300

    Cf.f95

--- Cf.f95
+++ Cf.f95
@@ -1,4 +1,4 @@
-subroutine Cf(NI, NJ, dy, C_f, U, mu, r0, u0)
+subroutine Friction_coefficient(NI, NJ, dy, Cf, U, MU, R0, U0)
 implicit none
 integer :: NI, NJ, i
 real :: dy, mu, r0, u0
@@ -6,7 +6,7 @@ real, dimension(0 : NI, 0 : NJ) :: U
 real, dimension(0 : NI) :: Cf
 
 do i = 0, NI
-	Cf(i) = -(mu * (3 * U(i, 1) - 4 * U(i, 2) + U(i, 3)) / (2 * dy)) / (0.5 * r0 * u0**2)
+Cf(i) = (2 * mu * (U(i, 1) - U(i, 0)) / (dy)) / (r0 * u0**2)
 end do
 
 end subroutine
